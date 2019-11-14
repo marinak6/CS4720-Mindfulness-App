7f283c90b3e9329c2dda1948db76d87a48d3535b
Marina Kun <mk3qa@virginia.edu>
2019-11-14 18:11:03 -0500

    Added icons for tab navigator

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import React from 'react';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { Ionicons, FontAwesome } from '@expo/vector-icons';
 import LoginScreen from './components/LoginScreen'
 import SignupScreen from './components/SignupScreen'
 import WelcomeScreen from './components/WelcomeScreen'
@@ -9,11 +11,38 @@ import CalendarScreen from './components/CalendarScreen'
 import JournalScreen from './components/JournalScreen'
 
 
-const AppStack = createBottomTabNavigator({
-  Home: HomeScreen,
-  Calendar: CalendarScreen,
-  Journal: JournalScreen,
-});
+const AppStack = createBottomTabNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: {
+        tabBarLabel: 'Home',
+        tabBarIcon: ({ tintColor }) => (
+          <Ionicons name="ios-home" color={tintColor} size={25} />
+
+        )
+      }
+    },
+    Journal: {
+      screen: JournalScreen,
+      navigationOptions: {
+        tabBarLabel: 'Journal',
+        tabBarIcon: ({ tintColor }) => (
+          <FontAwesome name="pencil" color={tintColor} size={25} />
+        )
+      }
+    },
+    Calendar: {
+      screen: CalendarScreen,
+      navigationOptions: {
+        tabBarLabel: 'Calendar',
+        tabBarIcon: ({ tintColor }) => (
+          <Ionicons name="ios-calendar" color={tintColor} size={25} />
+        )
+      }
+    }
+  }
+);
 
 const AuthStack = createStackNavigator(
   {
@@ -33,7 +62,7 @@ export default createAppContainer(
       Auth: AuthStack,
     },
     {
-      initialRouteName: 'Auth'
+      initialRouteName: 'App'
     }
   )
 )
