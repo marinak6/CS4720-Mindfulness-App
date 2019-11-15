e5801778d89541b3902c706bda64beb5b9ec7fc4
Yaman Shrestha <shresthayaman@hotmail.com>
2019-11-14 22:07:02 -0500

    Updated JournalScreen.js
    Added a scrollable textinput but testing using a rich text editor

--- components/JournalScreen.js
+++ components/JournalScreen.js
@@ -1,12 +1,56 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native'
+import Constants from 'expo-constants';
+
 class JournalScreen extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { entry: 'What happened toaday? '};
+    }
+
     render() {
         return (
-            <View>
-                <Text>Journal!!</Text>
+            <View style={styles.container}>
+                <Text style = {styles.title}>Today Date:</Text>
+                <View style={styles.line}/>
+                <TextInput 
+                    multiline 
+                    autogrow 
+                    scrollEnabled 
+                    style={styles.entry}>
+                        Journal!!
+                </TextInput>
+                <Button title="Add Journal Entry"/>
             </View>
         )
     }
 }
+
+let styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        flexDirection: 'column',
+        paddingTop: Constants.statusBarHeight,
+        alignItems: "center",
+      }, 
+      line:{
+        borderBottomColor: 'black',
+        borderBottomWidth: 10,
+        marginBottom: 10
+      },
+      title:{
+        fontFamily: 'BodoniSvtyTwoITCTT-Bold',
+        fontSize: 25,
+        color: '#383838'
+      },
+      entry:{
+          width: "100%",
+          padding: 10,
+          borderColor: 'black',
+          borderWidth: 2,
+      }
+
+})
+
 export default JournalScreen
